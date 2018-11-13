2dcb61a5150afddbe6859c2abca8fa56df741e69
Jayoung Lee <39401563+JayoungLee92@users.noreply.github.com>
2018-11-14 00:00:17 +0900

    first commit simple weather app

---
+++ App.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator} from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+    isLoaded: false
+  }
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+          {isLoaded ? null : (
+              <View style={styles.loading}>
+                <Text style={styles.loadingText}>Getting the fucking weather</Text>
+              </View>
+          )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  loading:{
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
+  }
+});
